efad8fcfb4462c40e23f2c070ea68352803815d5
Jose Peresini <joseignacioperesini@gmail.com>
2024-11-18 00:08:31 -0300

    delete function and applying rendering

--- 03-javascript-web/05-metodos-de-arreglos/assets/js/script.js
+++ 03-javascript-web/05-metodos-de-arreglos/assets/js/script.js
@@ -1,8 +1,9 @@
-const todoDivContainer = document.querySelector(".todo__div--container");
 const enterInput = document.querySelector(".todo__input");
 const clickButton = document.querySelector(".todo__button");
+const todoDivContainer = document.querySelector(".todo__div--container");
 const seeId = document.querySelector(".id__ul");
 const seeList = document.querySelector(".task__ul");
+const seeDelete = document.querySelector(".deletetask__ul");
 
 const tasks = [];
 
@@ -25,6 +26,7 @@ clickButton.addEventListener("click", () => {
 function renderHTML() {
 	let htmlTaskId = "";
 	let htmlTaskUl = "";
+	let htmlDeleteTask = "";
 
 	tasks.forEach((task, index) => {
 		htmlTaskId += `
@@ -35,15 +37,31 @@ function renderHTML() {
 
 		htmlTaskUl += `
 			<ul class="task__ul">
-				<li>${task}</li>
+				<li>${task.text}</li>
+			</ul>
+		`;
+
+		htmlDeleteTask += `
+			<ul class="deletetask__ul">
+				<li class="deletetask__li">
+					<button class="deletetask__button" onclick="deleteTask(${index})">
+						<i class="deletetask__i bi bi-x-lg"></i>
+					</button>
+				</li>
 			</ul>
 		`;
 	});
 
 	seeId.innerHTML = htmlTaskId;
 	seeList.innerHTML = htmlTaskUl;
+	seeDelete.innerHTML = htmlDeleteTask;
 
 	if (tasks.length >= 5) {
 		todoDivContainer.style.height = `${tasks.length * 2}em`;
 	}
+}
+
+function deleteTask(id) {
+	tasks.splice(id, 1);
+	renderHTML();
 }

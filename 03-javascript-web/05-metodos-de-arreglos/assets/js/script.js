5002b843f25b5d73f7da8e0d11c0425fb0b5e187
Jose Peresini <joseignacioperesini@gmail.com>
2024-11-18 00:01:13 -0300

    event click

--- 03-javascript-web/05-metodos-de-arreglos/assets/js/script.js
+++ 03-javascript-web/05-metodos-de-arreglos/assets/js/script.js
@@ -5,3 +5,19 @@ const seeId = document.querySelector(".id__ul");
 const seeList = document.querySelector(".task__ul");
 
 const tasks = [];
+
+clickButton.addEventListener("click", () => {
+	const newTask = enterInput.value;
+
+	if (newTask === "") {
+		enterInput.placeholder = "Campo requerido";
+		return;
+	} else {
+		enterInput.placeholder = "Ingrese una tarea";
+	}
+
+	tasks.push(newTask);
+	enterInput.value = "";
+
+	renderHTML();
+});

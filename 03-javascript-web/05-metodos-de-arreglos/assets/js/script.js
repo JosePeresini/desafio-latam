651d3fb244e093658a78cf6b43da0c309d3c20f7
Jose Peresini <joseignacioperesini@gmail.com>
2024-11-18 00:00:09 -0300

    select variables

---
+++ 03-javascript-web/05-metodos-de-arreglos/assets/js/script.js
@@ -0,0 +1,7 @@
+const todoDivContainer = document.querySelector(".todo__div--container");
+const enterInput = document.querySelector(".todo__input");
+const clickButton = document.querySelector(".todo__button");
+const seeId = document.querySelector(".id__ul");
+const seeList = document.querySelector(".task__ul");
+
+const tasks = [];

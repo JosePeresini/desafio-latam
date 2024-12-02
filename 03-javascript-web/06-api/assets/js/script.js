f8dc01c011821e187c5bcf19e5b1404229e7dd19
Jose Peresini <joseignacioperesini@gmail.com>
2024-12-01 22:52:29 -0300

    adding option tag to the dom

--- 03-javascript-web/06-api/assets/js/script.js
+++ 03-javascript-web/06-api/assets/js/script.js
@@ -2,9 +2,27 @@ async function getCurrency() {
 	try {
 		const res = await fetch("./assets/js/mindicador.json");
 		const data = await res.json();
-		renderCurrency(data);
+		renderCurrency(data.currency);
 		return data.currency;
 	} catch (error) {
 		console.log("Something went wrong", error.message);
 	}
+}
+
+async function renderCurrency(data) {
+	const conversorInput = document.querySelector(".conversor__input");
+	const conversorSelect = document.querySelector(".conversor__select");
+	const conversorSpanV = document.querySelector(".conversor__span--value");
+	const conversorButton = document.querySelector(".conversor__button");
+	const conversorSpanR = document.querySelector(".conversor__span--result");
+
+	data.forEach((element) => {
+		const option = document.createElement("option");
+
+		option.value = element.code;
+		option.textContent = element.name;
+		option.dataset.value = element.measurement_value;
+		option.className = "conversor__option";
+		conversorSelect.appendChild(option);
+	});
 }

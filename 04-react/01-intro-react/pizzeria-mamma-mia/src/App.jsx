071b3327c645aaf7b860894fce1457cba87cc959
Jose Peresini <joseignacioperesini@gmail.com>
2024-12-15 19:23:31 -0300

    imports css

--- 04-react/01-intro-react/pizzeria-mamma-mia/src/App.jsx
+++ 04-react/01-intro-react/pizzeria-mamma-mia/src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import "./assets/css/App.css";
+
+function App() {
+	return (
+		<div className="container">
+			<Navbar />
+			<Home />
+			<Footer />
+		</div>
+	);
+}
 
 export default App;

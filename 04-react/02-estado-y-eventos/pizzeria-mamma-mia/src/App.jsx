d1c2a65ba7ff176cc2b1190964e2bb22dafd812c
Jose Peresini <joseignacioperesini@gmail.com>
2024-12-24 15:00:43 -0300

    creating the app and its main as react base

--- 04-react/02-estado-y-eventos/pizzeria-mamma-mia/src/App.jsx
+++ 04-react/02-estado-y-eventos/pizzeria-mamma-mia/src/App.jsx
@@ -1,7 +1,21 @@
-import "../src/assets/css/App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
+import "./assets/css/App.css";
 
 function App() {
-	return <></>;
+	return (
+		<div className="container">
+			<Navbar />
+			<Home />
+			<Register />
+			<Login />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- 04-react/02-estado-y-eventos/pizzeria-mamma-mia/src/main.jsx
+++ 04-react/02-estado-y-eventos/pizzeria-mamma-mia/src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App.jsx";
-
-import "../src/assets/css/reset.css";
-import "../src/assets/css/index.css";
+import App from "./App";
+import "./assets/css/reset.css";
+import "./assets/css/index.css";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>

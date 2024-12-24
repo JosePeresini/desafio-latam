9e3f4b5d9fd24e7e2fb4db811130126cc7626058
Jose Peresini <joseignacioperesini@gmail.com>
2024-12-24 14:45:49 -0300

    eliminating and adding imports

--- 04-react/02-estado-y-eventos/pizzeria-mamma-mia/src/main.jsx
+++ 04-react/02-estado-y-eventos/pizzeria-mamma-mia/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+
+import "../src/assets/css/reset.css";
+import "../src/assets/css/index.css";
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);

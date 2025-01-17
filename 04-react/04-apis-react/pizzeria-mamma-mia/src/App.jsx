15da75c826c4750997b7fcafb1459671673765ae
Jose Peresini <joseignacioperesini@gmail.com>
2025-01-17 00:36:30 -0300

    updated App component to include Navbar, Home, Pizza, and Footer; commented out Register, Login, and Cart

---
+++ 04-react/04-apis-react/pizzeria-mamma-mia/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Cart from "./components/Cart";
+import Pizza from "./components/Pizza";
+import Footer from "./components/Footer";
+import "./assets/css/App.css";
+
+function App() {
+	return (
+		<div className="container">
+			<Navbar />
+			<Home />
+			{/* <Register />
+			<Login />
+			<Cart /> */}
+			<Pizza />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;

944aea704bbd60c3c9c99770a877c55f57e6ee9a
Jose Peresini <joseignacioperesini@gmail.com>
2025-01-20 10:58:37 -0300

    commenting on the app component

--- 04-react/04-apis-react/pizzeria-mamma-mia/src/App.jsx
+++ 04-react/04-apis-react/pizzeria-mamma-mia/src/App.jsx
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Cart from "./components/Cart";
+// import Home from "./components/Home";
+// import Register from "./components/Register";
+// import Login from "./components/Login";
+// import Cart from "./components/Cart";
 import Pizza from "./components/Pizza";
 import Footer from "./components/Footer";
 import "./assets/css/App.css";
@@ -11,7 +11,7 @@ function App() {
 	return (
 		<div className="container">
 			<Navbar />
-			<Home />
+			{/* <Home /> */}
 			{/* <Register />
 			<Login />
 			<Cart /> */}

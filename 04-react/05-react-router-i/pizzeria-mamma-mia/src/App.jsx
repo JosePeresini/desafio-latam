bdad8db166cb5bd374a0772384ef8c231dc45a71
Jose Peresini <joseignacioperesini@gmail.com>
2025-01-25 14:29:08 -0300

    adding the routes for the new components

--- 04-react/05-react-router-i/pizzeria-mamma-mia/src/App.jsx
+++ 04-react/05-react-router-i/pizzeria-mamma-mia/src/App.jsx
@@ -1,20 +1,22 @@
 import Navbar from "./components/Navbar";
-// import Home from "./components/Home";
-// import Register from "./components/Register";
-// import Login from "./components/Login";
-// import Cart from "./components/Cart";
-import Pizza from "./components/Pizza";
 import Footer from "./components/Footer";
+
+import Home from "./pages/HomePage";
+import Pizza from "./pages/PizzaPage";
+import Cart from "./pages/CartPage";
+import Register from "./pages/RegisterPage";
+import Login from "./pages/LoginPage";
+
 import "./assets/css/App.css";
 
 function App() {
 	return (
 		<div className="container">
 			<Navbar />
-			{/* <Home /> */}
-			{/* <Register />
+			<Home />
+			<Register />
 			<Login />
-			<Cart /> */}
+			<Cart />
 			<Pizza />
 			<Footer />
 		</div>

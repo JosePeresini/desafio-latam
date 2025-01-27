5c79dc66d93b40b4c50a40be9cf33868518bcff9
Jose Peresini <joseignacioperesini@gmail.com>
2025-01-26 22:18:56 -0300

    new profile component

---
+++ 04-react/05-react-router-i/pizzeria-mamma-mia/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+const Profile = () => {
+	return (
+		<>
+			<section className="profile">
+				<div className="profile__div">
+					<img
+						src="./src/assets/image/user.png"
+						alt=""
+						className="profile__img"
+					/>
+					<p className="profile__p">mail@mail.com</p>
+				</div>
+				<a href="" className="profile__a">
+					Cerrar Sesiom
+				</a>
+			</section>
+		</>
+	);
+};
+
+export default Profile;

f80eb305bdafa3ee7c26316368b10815370af30e
Jose Peresini <joseignacioperesini@gmail.com>
2025-01-26 22:20:09 -0300

    new component not-found

--- 04-react/05-react-router-i/pizzeria-mamma-mia/src/pages/NotFound.jsx
+++ 04-react/05-react-router-i/pizzeria-mamma-mia/src/pages/NotFound.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
 	return (
 		<>
-			<h1>404</h1>
+			<section className="notfound">
+				<div className="notfound__div">
+					<h2 className="notfound__h2">
+						<span className="notfound__span">4</span>
+						<img
+							src="./src/assets/image/error-404.ico"
+							alt=""
+							className="notfound__img"
+						/>
+						<span className="notfound__span">4</span>
+					</h2>
+					<p className="notfound__p">Not Found</p>
+				</div>
+				<Link to={"/"} className="notfound__link">
+					Volver al inicio
+				</Link>
+			</section>
 		</>
 	);
 };

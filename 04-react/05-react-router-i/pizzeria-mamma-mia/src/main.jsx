f1b057c40fbcce6d81becdd619ab615bda5d2d2c
Jose Peresini <joseignacioperesini@gmail.com>
2025-01-26 22:22:33 -0300

    installing react-router dependency and adding its usage

--- 04-react/05-react-router-i/pizzeria-mamma-mia/src/main.jsx
+++ 04-react/05-react-router-i/pizzeria-mamma-mia/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./assets/css/reset.css";
 import "./assets/css/index.css";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</StrictMode>
 );

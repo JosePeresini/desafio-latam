77ad02cae71791c183b194d0b171a31fbf7c2314
Jose Peresini <joseignacioperesini@gmail.com>
2025-02-03 01:20:47 -0300

    add home page with pizza data

--- 04-react/06-context/pizzeria-mamma-mia/src/pages/HomePage.jsx
+++ 04-react/06-context/pizzeria-mamma-mia/src/pages/HomePage.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
-
 import CardPizza from "../components/CardPizza";
 import Header from "../components/Header";
-// import { pizzas } from "../assets/js/pizzas";
 
 const Home = () => {
 	const [info, setInfo] = useState([]);
@@ -23,41 +21,16 @@ const Home = () => {
 			<Header />
 			<section className="cardpizza">
 				<div className="cardpizza__div--container">
-					{
-						/* <CardPizza
-							name="Napolitana"
-							price={5950}
-							ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-							img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.co
-							m/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9
-							080-784dcc87ec2c"
-						/>
+					{info.map((pizza) => (
 						<CardPizza
-							name="Española"
-							price={6950}
-							ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-							img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.co
-							m/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-
-							a1c6-8c57bc388fab"
+							key={pizza.id}
+							id={pizza.id}
+							name={pizza.name}
+							price={pizza.price}
+							ingredients={pizza.ingredients}
+							img={pizza.img}
 						/>
-						<CardPizza
-							name="Pepperoni"
-							price={6950}
-							ingredients={["mozzarella", "pepperoni", "orégano"]}
-							img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.co
-							m/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-
-							ac54-90f6c31eb3e3"
-						/> */
-						info.map((pizza) => (
-							<CardPizza
-								key={pizza.id}
-								name={pizza.name}
-								price={pizza.price}
-								ingredients={pizza.ingredients}
-								img={pizza.img}
-							/>
-						))
-					}
+					))}
 				</div>
 			</section>
 		</>

3536bdbb812c2cd21e676893826acb218ec0aa8b
Jose Peresini <joseignacioperesini@gmail.com>
2025-02-10 01:18:22 -0300

    implement useparams to get pizza id and fetch pizza information from api

--- 04-react/07-react-router-ii/pizzeria-mamma-mia/src/pages/PizzaPage.jsx
+++ 04-react/07-react-router-ii/pizzeria-mamma-mia/src/pages/PizzaPage.jsx
@@ -1,50 +1,53 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const Pizza = () => {
-	const [info, setInfo] = useState([]);
+const PizzaPage = () => {
+	const { id } = useParams();
+	const [info, setInfo] = useState(null);
 
 	useEffect(() => {
-		dataPizza();
-	}, []);
+		const fetchPizza = async () => {
+			const url = `http://localhost:5000/api/pizzas/${id}`;
+			const res = await fetch(url);
+			const data = await res.json();
+			setInfo(data);
+		};
+		fetchPizza();
+	}, [id]);
 
-	const dataPizza = async () => {
-		const url = "http://localhost:5000/api/pizzas/p001";
-		const res = await fetch(url);
-		const data = await res.json();
-		setInfo(data);
-	};
+	if (!info) {
+		return <p>Loading...</p>;
+	}
 
 	return (
-		<>
-			<section className="pizza">
-				<article className="pizza__article">
-					<header className="pizza__header">
-						<h3 className="pizza__h3">Pizza {info.name}</h3>
-						<p className="pizza__span"> ${info.price}</p>
-					</header>
-					<p className="pizza__p">
-						Ingredientes:
-						<span className="pizza__span--ing">
-							{info.ingredients && info.ingredients.join(", ")}
-						</span>
-					</p>
+		<section className="pizza">
+			<article className="pizza__article">
+				<header className="pizza__header">
+					<h3 className="pizza__h3">Pizza {info.name}</h3>
+					<p className="pizza__span"> ${info.price}</p>
+				</header>
+				<p className="pizza__p">
+					Ingredientes:
+					<span className="pizza__span--ing">
+						{info.ingredients && info.ingredients.join(", ")}
+					</span>
+				</p>
+				<img
+					src={info.img}
+					alt={`pizza img ` + info.name}
+					className="pizza__img"
+				/>
+				<footer className="pizza__footer">
 					<img
 						src={info.img}
 						alt={`pizza img ` + info.name}
-						className="pizza__img"
+						className="pizza__img--desc"
 					/>
-					<footer className="pizza__footer">
-						<img
-							src={info.img}
-							alt={`pizza img ` + info.name}
-							className="pizza__img--desc"
-						/>
-						<p className="pizza__p--desc">{info.desc}</p>
-					</footer>
-				</article>
-			</section>
-		</>
+					<p className="pizza__p--desc">{info.desc}</p>
+				</footer>
+			</article>
+		</section>
 	);
 };
 
-export default Pizza;
+export default PizzaPage;
